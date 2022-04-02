c876224dc797e7eafccfaa86a5f2d55a03c6148c
AutoAFK <ranhaim85@gmail.com>
2022-04-02 16:53:32 +0300

    Added write and read from files functions with a test and todo list

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
-package hostswriter
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const trial string = "trial.txt"
+
+func writeToFile() {
+	f, err := os.OpenFile(trial, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	d := "A new line to the file\n"
+	_, err = fmt.Fprint(f, d)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func readFile() {
+	data, err := os.ReadFile("trial.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	for i := 0; i < 100; i++ {
+		writeToFile()
+	}
+	readFile()
 }
